0e8432a6d55e6c4a37d37f7a2dec641829b50bd4
Alexis <alexisd195@gmail.com>
2021-09-29 02:19:55 +0200

    Modifs + arrangements design

--- frontend/view/home/home.js
+++ frontend/view/home/home.js
@@ -6,16 +6,16 @@ fetch("http://localhost:3000/api/cameras")
         console.log(jsonListArticle);
         for (let jsonArticle of jsonListArticle){
             let article = new ArticleManager(jsonArticle);
-            document.querySelector(".hey").innerHTML += `
-                <div class="card m-5 col-4">
-                    <div class="card-header">
+            document.querySelector(".articles-home").innerHTML += `
+                <div class="card m-5 col-md-4">
+                    <div class="card-header bg-title-cards-home">
                         <a href="http://127.0.0.1:5500/frontend/view/article/article.html?id=${article._id}">
                             <h3 class="card-title d-flex justify-content-center">${article.name}</h3>
                         </a>
                     </div>
                     <img src="${article.imageUrl}" class="card-img-top" />
                     <div class="card-body">
-                        <p class="card-text text-center p-4">${article.description}</p>
+                        <p class="card-text text-center p-md-4">${article.description}</p>
                         <p class="card-text text-center">${article.price / 1000}${0}<sup>€</sup></p>
                         <a href="http://127.0.0.1:5500/frontend/view/article/article.html?id=${article._id}">lien vers l'article</a>
                     </div>
